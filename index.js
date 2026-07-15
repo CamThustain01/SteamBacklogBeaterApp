4ebf080e10b9ef8e0e7f3b407adadefd3650575f
Cameron Thustain <thustain2@hotmail.co.uk>
2026-07-15 23:04:57 +0100

    testing game array

--- index.js
+++ index.js
@@ -1,1 +1,58 @@
-console.log('Happy developing ✨')
+
+const game_array =[
+    {
+    "appid": 205100,
+    "name": "Dishonored",
+    "playtime_forever": 0,
+    "img_icon_url": "74f8ee1ba536e0759e64a9bf801fc013e16c8dd1",
+    "has_community_visible_stats": true,
+    "playtime_windows_forever": 0,
+    "playtime_mac_forever": 0,
+    "playtime_linux_forever": 0,
+    "playtime_deck_forever": 0,
+    "rtime_last_played": 0,
+    "has_leaderboards": true,
+    "playtime_disconnected": 0
+    },
+    {
+        "appid": 1030300,
+        "name": "Hollow Knight: Silksong",
+        "playtime_forever": 2293,
+        "img_icon_url": "b4a999c1302e3ac123c041fd41bb8a34528c6ab5",
+        "has_community_visible_stats": true,
+        "playtime_windows_forever": 0,
+        "playtime_mac_forever": 2,
+        "playtime_linux_forever": 2291,
+        "playtime_deck_forever": 2291,
+        "rtime_last_played": 1761598874,
+        "playtime_disconnected": 0
+    },
+    {
+        "appid": 22380,
+        "name": "Fallout: New Vegas",
+        "playtime_forever": 2217,
+        "img_icon_url": "1711fd8c46d739feec76bd4a64eaeeca5b87e3a7",
+        "has_community_visible_stats": true,
+        "playtime_windows_forever": 0,
+        "playtime_mac_forever": 0,
+        "playtime_linux_forever": 2217,
+        "playtime_deck_forever": 2217,
+        "rtime_last_played": 1766098764,
+        "playtime_disconnected": 114
+    },
+    {
+        "appid": 1621690,
+        "name": "Core Keeper",
+        "playtime_forever": 1131,
+        "img_icon_url": "53517a1b2eb22e22ac4a1f71c814209f1b84c063",
+        "has_community_visible_stats": true,
+        "playtime_windows_forever": 814,
+        "playtime_mac_forever": 0,
+        "playtime_linux_forever": 316,
+        "playtime_deck_forever": 316,
+        "rtime_last_played": 1779398262,
+        "playtime_disconnected": 0
+    },
+    ];
+
+console.log(game_array);
